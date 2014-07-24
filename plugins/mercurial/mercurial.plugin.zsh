374f7ea88fcddffaf0e16df2cc971403e4fd5aed
Alec Bedzir <albed@smile.fr>
2014-07-24 11:24:06 +0300

    Added mercurial plugin and theme changed

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -64,3 +64,10 @@ function hgic() {
 function hgoc() {
     hg outgoing "$@" | grep "changeset" | wc -l
 }
+
+function hg_prompt_info {
+    hg prompt --angle-brackets "\
+<%{$fg_bold[blue]%}hg:(%{$fg_bold[red]%}<branch>><:<tags|, >%{$fg_bold[blue]%})>\
+%{$fg[yellow]%}<status|modified|unknown><update>\
+<patches: <patches|join( → )>>%{$reset_color%}" 2>/dev/null
+}
